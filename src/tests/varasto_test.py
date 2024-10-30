d395ad53dc4ad5baff4d54320a8ed84720ba7b66
Jenni Jokinen <153839491+jokijen@users.noreply.github.com>
2024-10-30 12:05:42 +0200

    Otettu käyttöön Poetry ja testaus

--- src/tests/varasto_test.py
+++ src/tests/varasto_test.py
@@ -10,8 +10,27 @@ class TestVarasto(unittest.TestCase):
         # https://docs.python.org/3/library/unittest.html#unittest.TestCase.assertAlmostEqual
         self.assertAlmostEqual(self.varasto.saldo, 0)
 
+    # UUSI
+    def test_palautetaan_oikea_merkkijonoesitys(self):
+        self.assertEqual(str(self.varasto), "saldo = 0, vielä tilaa 10")
+    
+    # UUSI
+    def test_uudella_varastolla_ei_negatiivista_alkusaldoa(self):
+        varasto = Varasto(10, -1)
+        self.assertAlmostEqual(varasto.saldo, 0.0)
+
     def test_uudella_varastolla_oikea_tilavuus(self):
         self.assertAlmostEqual(self.varasto.tilavuus, 10)
+
+    # UUSI
+    def test_uudella_varastolla_ei_negatiivista_tilavuutta(self):
+        varasto = Varasto(-1)
+        self.assertAlmostEqual(varasto.tilavuus, 0.0)
+
+    # UUSI
+    def test_ei_voi_lisata_varastoon_negatiivista_maaraa(self):
+        self.varasto.lisaa_varastoon(-1)
+        self.assertAlmostEqual(self.varasto.saldo, 0)
 
     def test_lisays_lisaa_saldoa(self):
         self.varasto.lisaa_varastoon(8)
@@ -23,6 +42,22 @@ class TestVarasto(unittest.TestCase):
 
         # vapaata tilaa pitäisi vielä olla tilavuus-lisättävä määrä eli 2
         self.assertAlmostEqual(self.varasto.paljonko_mahtuu(), 2)
+
+    # UUSI
+    def test_ei_voi_lisata_varastoon_tilavuutta_isompaa_maaraa(self):
+        self.varasto.lisaa_varastoon(11)
+        self.assertAlmostEqual(self.varasto.saldo, 10)
+
+    # UUSI
+    def test_ei_voi_ottaa_negatiivista_maaraa_varastosta(self):
+        saatu_maara = self.varasto.ota_varastosta(-1)
+        self.assertAlmostEqual(saatu_maara, 0.0)
+
+    # UUSI
+    def test_ei_voi_ottaa_saldoa_isompaa_maaraa_varastosta(self):
+        self.varasto.lisaa_varastoon(10)
+        saatu_maara = self.varasto.ota_varastosta(11)
+        self.assertAlmostEqual(saatu_maara, 10)
 
     def test_ottaminen_palauttaa_oikean_maaran(self):
         self.varasto.lisaa_varastoon(8)
